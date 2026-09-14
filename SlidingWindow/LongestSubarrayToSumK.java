f0a6804d306472424829670276df9b143cb44484
RiyaV2024 <riyaverma4065@gmail.com>
2026-09-14 21:06:12 +0530

    Longest Subarray Sum K

---
+++ SlidingWindow/LongestSubarrayToSumK.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int longestSubarray(int[] arr, int k) {
+        int i = 0;
+        int j = 0;
+        int sum = 0;
+        int maxLen = 0;
+        while (j < arr.length) {
+            sum += arr[j];
+            while (sum > k) {
+                sum -= arr[i];
+                i++;
+            }
+            if (sum == k) {
+                maxLen = Math.max(maxLen, j - i + 1);
+            }
+            j++;
+        }
+        return maxLen;
+    }
+}
