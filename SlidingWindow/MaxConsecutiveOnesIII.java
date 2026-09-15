5c6506db5a1cc85b980db39bd24b68cbdf7c99e8
RiyaV2024 <riyaverma4065@gmail.com>
2026-09-15 20:44:53 +0530

    Max Consecutive Ones 3

---
+++ SlidingWindow/MaxConsecutiveOnesIII.java
@@ -0,0 +1,21 @@
+
+class Solution {
+    public int longestOnes(int[] nums, int k) {
+        int maxLen = 0, l = 0, r = 0, zeroes = 0;
+        while (r < nums.length) {
+            if (nums[r] == 0) {
+                zeroes++;
+            }
+            if (zeroes > k) {
+                if (nums[l] == 0) {
+                    zeroes--;
+                }
+                l++;
+            }
+            int len = r - l + 1;
+            maxLen = Math.max(maxLen, len);
+            r++;
+        }
+        return maxLen;
+    }
+}
